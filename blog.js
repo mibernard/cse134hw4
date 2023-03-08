d010fe16ca150092abc4a819981876ff0e4592f8
Matthew <matthewbernard4@gmail.com>
2023-03-08 06:05:25 -0800

    tings

--- blog.js
+++ blog.js
@@ -1,1 +1,59 @@
+import { cstmConfirm } from './customdialog.js'
 
+export var posts = [];
+
+export function deletePost(postID) {
+    let dialog = cstmConfirm('Delete post?');
+    dialog.addEventListener('close', function () {
+        if(this.returnValue) {
+            posts.splice(postID,1);
+            renderPosts();
+        }
+    })
+}
+
+export function editPost(postID) {
+    if (postID) {
+        let post = posts[postID];
+        document.getElementById('title').value = post.title;
+        document.getElementById('date').value = Number(post.date);
+        document.getElementById('summary') = post.summary;
+        document.getElementById('postID').value = postID;
+    }
+    document.querySelector('#addDialog').showModal();
+}
+
+export function addPost() {
+    let addDialog = document.getElementById("addDialog");
+        
+    document.body.appendChild(addDialog);
+    addDialog.showModal();   
+
+}
+
+export function renderPosts() {
+    let markup = '';
+    if(posts.length===0) 
+        markup = 'No posts to display';
+    else {
+        markup = `
+        <ul>
+        ${posts.map((post,index) => `
+        <li>${post.title} (${post.date}) - Summary: ${post.summary}
+        <button onclick="editPost(${index})">Edit</button>
+        <button onclick="deletePost(${index})>Delete</button>
+        </li>`
+        ).join('')}
+        </ul>`;
+    }
+    document.querySelector('#postList').innerHTML = markup;
+}
+
+export function resetAll() {}
+
+export function setUp() {
+    
+    document.getElementById('addBtn').addEventListener('click', function() {
+        let dialog = addPost();
+    }); 
+}
--- customdialog.js
+++ customdialog.js
@@ -1,1 +1,53 @@
+export function cstmAlert(s) {
+  let alertDialog = document.createElement("dialog");
+  alertDialog.innerHTML = `
+    <p>${s}</p>
+    <div style="text-align: center">
+    <button id="okBtn">Ok</button>
+    </div>
+    `;
+  document.body.appendChild(alertDialog);
 
+  alertDialog.querySelector("#okBtn").onclick = function () {
+    alertDialog.close();
+  };
+
+  alertDialog.showModal();
+}
+
+export function cstmConfirm(s) {
+  let confirmDialog = document.createElement("dialog");
+  confirmDialog.innerHTML = `
+    <p>${s}</p>
+    <form method="dialog">
+    <div>
+    <button type="submit" id="cancelBtn" value="">Cancel</button>
+    <button type="submit" id="okBtn" value="true">Ok</button>
+    </div>
+    </form>
+    `;
+  document.body.appendChild(confirmDialog);
+  confirmDialog.showModal();
+  return confirmDialog;
+}
+
+export function cstmPrompt(str) {
+  let promptDialog = document.createElement("dialog");
+
+  promptDialog.innerHTML = `
+    <form method = "dialog">
+    <div>
+    <label>${str}</label>
+    </div>
+    <input type="text" id="promptResponse" value="" size="60"
+    <div style="text-align: right">
+    <button type="submit" id="cancelBtn" value="">Cancel</button>
+    <button type="submit" id="okBtn" value="true">Ok</button>
+    </div>
+    </form>
+    `;
+
+  document.body.appendChild(promptDialog);
+  promptDialog.showModal();
+  return promptDialog;
+}
